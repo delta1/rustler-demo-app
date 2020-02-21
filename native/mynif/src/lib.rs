6fde2dba87f9600675afbe3a81fcc2e8ab7c6b25
Byron Hambly <byron@clockboot.com>
2020-02-21 13:31:34 +0200

    comments

--- native/mynif/src/lib.rs
+++ native/mynif/src/lib.rs
@@ -14,8 +14,13 @@ fn panic() {
 
 #[rustler::nif]
 fn crash() {
+    // create a null pointer
     let p: *const i32 = ptr::null();
+    // https://doc.rust-lang.org/book/ch19-01-unsafe-rust.html
     unsafe {
+        // dereferencing a raw pointer can only be done in an unsafe block
+        // but dereferencing a NULL pointer is undefined behavior!
+        // and likely to cause a segmentation fault and program crash
         println!("{:?}", *p);
     }
 }
